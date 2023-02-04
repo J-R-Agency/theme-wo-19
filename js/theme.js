4241bbeb2cb7d445b309e440526840f4b9b820e7
Steve <steve@igoo.co.uk>
2023-02-04 21:35:16 +0000

    mobile overrides

--- js/theme.js
+++ js/theme.js
@@ -6131,14 +6131,50 @@
     }, false);
   }
 })();
-/**
- * File custom-javascript.js.
- *
- */
-(function () {
-  $('dropdown-toggle').click(false);
-  $('.dropdown-menu.show .menu-item-has-children.dropdown').on('click', function (e) {
+window.addEventListener("load", event => {
+  // try to override issues with bootstrap js
+  let dropdownDropdownToggles = document.querySelectorAll('.dropdown-menu .menu-item-has-children');
+  if (dropdownDropdownToggles) {
+    console.log(dropdownDropdownToggles);
+    dropdownDropdownToggles.forEach(i => {
+      i.addEventListener('click', decideActionForDopdown);
+    });
+  }
+  function decideActionForDopdown(e) {
     e.preventDefault();
-    console.log(e, e.target);
-  });
-})();
+    let target = e.target;
+    let windowWidth = window.innerWidth;
+    console.log(e.target, windowWidth, e.target.tagName);
+    if (windowWidth >= 1025) {
+      if (e.target.tagName == 'A') {
+        window.location = target.getAttribute('href');
+      } else {
+        let link = target.querySelector('> a');
+        window.location = link.getAttribute('href');
+      }
+    } else {
+      let childDropdown = target.querySelector('.dropdown-menu');
+      let parentDropdown = target.parentElement;
+      let parent = target.parentElement.parentElement;
+      if (e.target.tagName == 'A') {
+        parent = parent.parentElement;
+        parentDropdown = target.parentElement.parentElement;
+        childDropdown = target.parentElement.querySelector('.dropdown-menu');
+      }
+      console.log(childDropdown, parent, parentDropdown);
+      if (childDropdown.classList.contains('show')) {
+        setTimeout(() => {
+          childDropdown.classList.remove('show');
+          parent.classList.add('show');
+          parentDropdown.classList.add('show');
+        }, 1);
+      } else {
+        setTimeout(() => {
+          parent.classList.add('show');
+          childDropdown.classList.add('show');
+          parentDropdown.classList.add('show');
+        }, 1);
+      }
+    }
+  }
+});
